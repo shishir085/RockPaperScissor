369a7f7b1b4a430d19d1efcb8f4874569704173e
Shishir Pandey <122428854+shishir085@users.noreply.github.com>
2023-12-28 19:56:53 +0545

    Add files via upload

--- app.js
+++ app.js
@@ -2,10 +2,35 @@ let userscore = 0
 let compscore = 0
 
 const choices = document.querySelectorAll(".choice")
+let msg = document.querySelector("#message")
+let  userscorepara=document.querySelector("#userscore")
+let  compscorepara=document.querySelector("#compscore")
 
+
+const showwinner=(userwin)=>{
+if(userwin)
+{
+    userscore++;
+    msg.innerText="You Win!"
+    msg.style.backgroundColor="green"
+    userscorepara.innerText=userscore;
+}
+else{
+    compscore++;
+    msg.innerText="You Lose!"
+    msg.style.backgroundColor="red"
+    compscorepara.innerText=compscore;
+
+
+}
+}
 
 let drawgame=()=>{
-    console.log('game was draw');
+    
+    msg.innerText="Game Was Draw!"
+    msg.style.backgroundColor="#4657B9"
+
+
 }
 const gencompchoice = () => {
     let options = ['rock', 'paper', 'scissors']
@@ -15,13 +40,28 @@ const gencompchoice = () => {
 }
 
 const playgame = (userchoice) => {
-    console.log("user choice =", userchoice);
+    console.log( userchoice);
     const compchoice = gencompchoice();
-    console.log("comp choice =", compchoice);
+    console.log( compchoice);
 
     if (userchoice===compchoice){
         drawgame();
     }
+    else{
+        let userwin=true;
+        if(userchoice==="rock"){
+            userwin=compchoice==="paper"?false:true;
+        }
+        else if(userchoice==="paper"){
+            userwin=compchoice==="scissors"?false:true;
+        }
+        else{
+            userwin=compchoice==="rock"?false:true;
+        }
+        showwinner(userwin);
+    }
+
+
 
 }
 
